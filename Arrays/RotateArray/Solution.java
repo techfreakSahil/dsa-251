d6db7e33b9a98aeff82a755a177aa435690b63dd
techfreakSahil <gm6504@myamu.ac.in>
2023-06-20 15:56:21 +0530

    ques.2.soln

---
+++ Arrays/RotateArray/Solution.java
@@ -0,0 +1,38 @@
+package Arrays.RotateArray;
+
+import java.util.*;
+
+class Solution {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int[] arr = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            int j;
+            int first = arr[0];
+
+            for (j = 0; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+
+            arr[j] = first;
+        }
+
+        for (int i : arr) {
+
+            System.out.print(i + " ");
+
+        }
+    }
+}
