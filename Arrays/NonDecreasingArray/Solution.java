2e0dc7d0890b581a5b048d0b474830780911aa80
techfreakSahil <gm6504@myamu.ac.in>
2023-06-20 23:02:40 +0530

    ques.3.soln

---
+++ Arrays/NonDecreasingArray/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length && count <= 1; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+                if (i < 2 || nums[i] >= nums[i - 2]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+        return count <= 1;
+    }
+}
