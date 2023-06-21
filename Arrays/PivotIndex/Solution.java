2646a2ddf505da8593c958283ea423dd614ab5aa
techfreakSahil <gm6504@myamu.ac.in>
2023-06-21 15:18:59 +0530

    arrays.4.ques.sol

---
+++ Arrays/PivotIndex/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+        int leftSum = 0, rightSum = 0, ans = -1;
+        for(int el: nums){
+            rightSum+=el;
+        }
+        for(int i=0;i<n;i++){
+            rightSum-=nums[i];
+            if(leftSum==rightSum){
+                ans = i;
+                break;
+            }
+            leftSum+=nums[i];
+        }
+        return ans;
+    }
+}
