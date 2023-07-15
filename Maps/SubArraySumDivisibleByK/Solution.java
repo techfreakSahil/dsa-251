2d0fe1022053afe717e43b79fe5ae79581fa969a
techfreakSahil <gm6504@myamu.ac.in>
2023-07-15 18:43:08 +0530

    map.4.ques.sol

---
+++ Maps/SubArraySumDivisibleByK/Solution.java
@@ -0,0 +1,19 @@
+package Maps.SubArraySumDivisibleByK;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int ans = 0;
+        int sum = 0;
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int left = (sum % k + k) % k;
+            ans += mp.getOrDefault(left, 0);
+            mp.put(left, mp.getOrDefault(left, 0) + 1);
+        }
+        return ans;
+    }
+}
