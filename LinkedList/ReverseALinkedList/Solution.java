a80e92b6b757cdcc1ee3c6737a30757f74ae8200
techfreakSahil <gm6504@myamu.ac.in>
2024-01-07 19:17:19 +0530

    rvrselinkedlist

---
+++ LinkedList/ReverseALinkedList/Solution.java
@@ -0,0 +1,15 @@
+package LinkedList.ReverseALinkedList;
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+}
