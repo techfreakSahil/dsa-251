c4332955e325cf6ea9486ee268e48329481c463f
techfreakSahil <gm6504@myamu.ac.in>
2024-01-07 19:18:56 +0530

    midpointinlist

---
+++ LinkedList/MidPointInLinkedList/Solution.java
@@ -0,0 +1,13 @@
+package LinkedList.MidPointInLinkedList;
+
+public class Solution {
+    public ListNode midPoinListNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
