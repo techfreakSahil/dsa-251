3d6d8abbcf4fec4d5c420f42712ad9594dcd4582
techfreakSahil <gm6504@myamu.ac.in>
2024-02-19 17:42:59 +0530

    java solution added

---
+++ BitsManipulation/PowerOfTwo/Solution.java
@@ -0,0 +1,9 @@
+package BitsManipulation.PowerOfTwo;
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        return (n & (n - 1)) == 0;
+    }
+}
