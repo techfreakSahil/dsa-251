1e51d3a7ddab45f37ec60a5bed655b9afd674f3d
techfreakSahil <gm6504@myamu.ac.in>
2024-03-12 01:57:44 +0530

    custom sort string question added

---
+++ Arrays/CustomSortString/Solution.java
@@ -0,0 +1,25 @@
+package Arrays.CustomSortString;
+
+public class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) {
+            char c = order.charAt(i);
+            while (freq[c - 'a']-- > 0) {
+                sb.append(c);
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (freq[i]-- > 0) {
+                sb.append((char) ('a' + i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
