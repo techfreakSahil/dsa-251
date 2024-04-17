54133e4c267b9ce8e0e103bd466837b118bba2ce
techfreakSahil <gm6504@myamu.ac.in>
2024-04-17 22:14:39 +0530

    added 2d matrix search

--- Arrays/PivotIndex/Solution.java
+++ Arrays/PivotIndex/Solution.java
@@ -1,17 +1,17 @@
-class Solution {
+public class Solution {
     public int findMiddleIndex(int[] nums) {
         int n = nums.length;
         int leftSum = 0, rightSum = 0, ans = -1;
-        for(int el: nums){
-            rightSum+=el;
+        for (int el : nums) {
+            rightSum += el;
         }
-        for(int i=0;i<n;i++){
-            rightSum-=nums[i];
-            if(leftSum==rightSum){
+        for (int i = 0; i < n; i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
                 ans = i;
                 break;
             }
-            leftSum+=nums[i];
+            leftSum += nums[i];
         }
         return ans;
     }
---
+++ MultiArray/Searchin2DArray/question.md
@@ -0,0 +1,10 @@
+## Problem Statement
+
+You are given an `m x n` integer matrix `matrix` with the following two properties:
+
+1. Each row is sorted in non-decreasing order.
+2. The first integer of each row is greater than the last integer of the previous row.
+
+Given an integer `target`, return `true` if `target` is in `matrix` or `false` otherwise.
+
+You must write a solution in `O(log(m * n))` time complexity.
