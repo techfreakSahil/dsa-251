88b6762dfd19268a7069c5213fd1c96a13069a7c
techfreakSahil <gm6504@myamu.ac.in>
2024-04-18 22:16:51 +0530

    array III part 3 majority element

---
+++ Arrays/MajorityElement/Solution.java
@@ -0,0 +1,19 @@
+package Arrays.MajorityElement;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int maj = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                maj = nums[i];
+                count++;
+            } else if (maj == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return maj;
+    }
+}
